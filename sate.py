480caac238b9bb114f4fb846b607d37d62cce5b2
Russell Jarvis <colouredstatic@gmail.com>
2018-04-24 17:04:58 -0700

    for open source community bug tracking

--- sate.py
+++ sate.py
@@ -18,10 +18,12 @@ import elephant
 import numpy as np
 
 
-def te(mdf1):
+def te():
     import numpy as np
     from idtxl.multivariate_te import MultivariateTE
     from idtxl.data import Data
+    import pickle
+
     n_procs = 1
     settings = {
         'cmi_estimator': 'JidtDiscreteCMI',
@@ -30,12 +32,21 @@ def te(mdf1):
         'max_lag_sources': 5,
         'min_lag_sources': 4}
     settings['cmi_estimator'] = 'JidtDiscreteCMI'
+    '''
     binary_trains = []
     for spiketrain in mdf1.spiketrains:
         x = conv.BinnedSpikeTrain(spiketrain, binsize=5 * pq.ms, t_start=0 * pq.s)
         binary_trains.append(x.to_array())
     print(binary_trains)
+    with open('tedat.p','wb') as f:
+        pickle.dump(binary_trains,f)
+    '''
+
+    with open('binary_trains.p','rb') as f:
+        binary_trains = pickle.load(f)
+
     dat = Data(np.array(binary_trains), dim_order='spr')
+
     dat.n_procs = n_procs
     settings = {'cmi_estimator': 'JidtKraskov',
             'max_lag_sources': 3,
@@ -48,8 +59,9 @@ def te(mdf1):
     # generate graph plots
     g_single = visualise_graph.plot_selected_vars(res_single, mte)
     g_full = visualise_graph.plot_network(res_full)
-
+'''
 iter_distances = natsorted(glob.glob('pickles/qi*.p'))
-mdfl = pickle.load(open(iter_distances[0],'rb'))
+mdf1 = pickle.load(open(iter_distances[0],'rb'))
 mdfloop = {}
-te(mdfl)
+'''
+te()
